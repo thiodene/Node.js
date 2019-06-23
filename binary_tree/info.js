b33c701aa66befa77f749f09804cbcd7a7285dc1
Serge Ayissi <ayissi.serge@gmail.com>
2019-06-22 22:22:39 -0400

    Update info.js

--- binary_tree/info.js
+++ binary_tree/info.js
@@ -1,3 +1,4 @@
 # Source:
 https://www.slideserve.com/payton/trees
 https://www.hackerrank.com/challenges/30-binary-search-trees/problem
+https://app.codility.com/programmers/lessons/99-future_training/tree_height/
