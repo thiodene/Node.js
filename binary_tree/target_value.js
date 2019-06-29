2366434137894b87721a7efeff6f1ad565ddd179
Serge Ayissi <ayissi.serge@gmail.com>
2019-06-29 16:54:31 -0400

    Create target_value.js

---
+++ binary_tree/target_value.js
@@ -0,0 +1,19 @@
+    //---------------------------------------------------------------------------------------------
+    this.nodesum = [] ;
+    this.nodeSummation = function(root,target)
+    {
+        let rootval ;
+        if (root === null)
+            return;
+
+        rootval = root.data;
+
+        this.nodeSummation(root.left,target);
+        this.nodeSummation(root.right,target);
+
+        if(this.nodesum.indexOf(target))
+            return true;
+        else
+            return false;
+
+    }
