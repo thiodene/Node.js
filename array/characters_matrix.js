44e432ffe60c112d4c77a0b19ef8eab91dc7f7aa
Serge Ayissi <ayissi.serge@gmail.com>
2019-07-20 23:07:29 -0400

    Create characters_matrix.js

---
+++ array/characters_matrix.js
@@ -0,0 +1,27 @@
+'use strict';
+// Function Search for word inside of matrix of characters
+function countWords(word){
+
+
+
+}
+
+
+
+// Functiona main and all the details inside
+function main()
+{
+
+
+
+
+
+
+
+}
+
+
+
+// MAIN -----------------------------------------------------------------------
+
+main();
