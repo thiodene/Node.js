ce845da5c98128d8ae38acc36948377d1c37e450
Serge Ayissi <ayissi.serge@gmail.com>
2019-07-23 08:27:00 -0400

    Create path_normalize.js

---
+++ string/path_normalize.js
@@ -0,0 +1,33 @@
+'use strict';
+// Function to normalize a path as absolute
+function pathNormalize(pathToNorm)
+{
+
+  var path = require('path');
+
+
+ return path.normalize(pathToNorm);
+
+}
+
+
+// Main function to be executed
+function main()
+{
+
+//var pathToNorm = "Users/Refsnes/../Jackson";
+//var pathToNorm = "/home/myself/node/wwwroot/static_files/gif/image.gif";
+var pathToNorm = "../Guide/././API/../../../Firefox";
+//var pathToNorm = "asdf//asdf//../qwer";
+//var pathToNorm = "/asdf//asdf//../qwer/..";
+//var pathToNorm = "asdf//asdf//asdf//asdf//asdf//../../../qwer";
+
+console.log(pathNormalize(pathToNorm));
+
+}
+
+
+
+// MAIN-----------------------------------------------------------------
+
+main();
