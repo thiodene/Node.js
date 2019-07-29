fd258f0c7eaa35e828c773b43cdcbb7ba06a7857
Serge Ayissi <ayissi.serge@gmail.com>
2019-07-28 22:59:14 -0400

    Create design_stack.js

---
+++ algorithm/design_stack.js
@@ -0,0 +1,28 @@
+'use strict';
+// Function to get the min imum of a stack
+getMinimum()
+{
+
+
+
+}
+
+popMin()
+{
+
+
+
+}
+
+// Build the main function
+main()
+{
+
+
+
+}
+
+
+// MAIN---------------------------------------------------------------------
+
+main();
