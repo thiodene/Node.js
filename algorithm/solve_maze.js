a718c59441f49622cc7191eadc2df55b81501cbc
Serge Ayissi <ayissi.serge@gmail.com>
2019-08-02 12:14:30 -0400

    Update solve_maze.js

--- algorithm/solve_maze.js
+++ algorithm/solve_maze.js
@@ -10,6 +10,11 @@ function solveMaze(maze)
   let currentPos = {};
   let posPath = [];
   let moveIter = 0;
+  let newPos = {};
+
+  // Take the limits of the maze in account
+  let maxI = maze.length - 1;
+  let maxJ = maze[0].length - 1;
 
   // Get the debut position  and position first
   for (let i=0;i<=maze.length - 1;i++)
@@ -42,30 +47,48 @@ function solveMaze(maze)
   else
     return false;
 
-
+  console.log(currentPos);
   // based on the last and current position go the opposite way!
   //if not possible change direction // New direction different from lastPos!
   while (currentPos != endPos)
   {
-    if (maze[currentPos.I+currentPos.I-lastPos.I][currentPos.J+currentPos.J-lastPos.J] == 0)
-    {
-      // make the move
-      waitlastPos = currentPos;
-      currentPos = {"I":currentPos.I+currentPos.I-lastPos.I,"J":currentPos.J+currentPos.J-lastPos.J};
-      lastPos = waitlastPos;
-      moveIter++;
 
-    }
-    else if(newPos = checkPossibleDir(maze,currentPos,lastPos))
+    if ( (currentPos.I+currentPos.I-lastPos.I >= 0 && currentPos.I+currentPos.I-lastPos.I <= maxI) && (currentPos.J+currentPos.J-lastPos.J >= 0 && currentPos.J+currentPos.J-lastPos.J <= maxJ) )
     {
-      // If new direction then make the move
-      waitlastPos = currentPos;
-      currentPos = {"I":newPos.I,"J":newPos.J};
-      lastPos = waitlastPos;
-      moveIter++;
+      if (maze[currentPos.I+currentPos.I-lastPos.I][currentPos.J+currentPos.J-lastPos.J] == 0)
+      {
+        // make the move
+        waitlastPos = currentPos;
+        currentPos = {"I":currentPos.I+currentPos.I-lastPos.I,"J":currentPos.J+currentPos.J-lastPos.J};
+        lastPos = waitlastPos;
+        moveIter++;
+
+      }
+      else if(newPos = checkPossibleDir(maze,currentPos,lastPos))
+      {
+        // If new direction then make the move
+        waitlastPos = currentPos;
+        currentPos = {"I":newPos.I,"J":newPos.J};
+        lastPos = waitlastPos;
+        moveIter++;
+      }
+      else
+        break;
     }
     else
-      break;
+    {
+      if(newPos = checkPossibleDir(maze,currentPos,lastPos))
+      {
+        // If new direction then make the move
+        waitlastPos = currentPos;
+        currentPos = {"I":newPos.I,"J":newPos.J};
+        lastPos = waitlastPos;
+        moveIter++;
+      }
+      else
+        break;
+    }
+    console.log(currentPos);
   }
 
   //checkPossibleDir();
@@ -73,23 +96,66 @@ function solveMaze(maze)
   return currentPos;
 }
 
-//function checkPossibleDir(maze,currentPos,lastPos)
-//{
+function checkPossibleDir(maze,currentPos,lastPos)
+{
   // Check for new position around current that isn't equal to last Position
   let x = 1;
+  let newPos = {};
 
-  // check if going right is okt
-  if (maze[currentPos.I][currentPos.J+x] == 0)
+  let maxI = maze.length - 1;
+  let maxJ = maze[0].length - 1;
+
+  // check if going down is ok
+  if (currentPos.I+x <= maxI)
   {
-
-
+    // Now check if this position is different from last pos
+    if (maze[currentPos.I+x][currentPos.J] == 0 && (currentPos.I+x != lastPos.I || currentPos.J != lastPos.J) )
+    {
+      newPos = {"I":currentPos.I+x,"J":currentPos.J};
+      return newPos;
+    }
   }
-  else
+
+
+  // check if going right is ok
+  if (currentPos.J+x <= maxJ)
   {
-
+    // Now check if this position is different from last pos
+    if (maze[currentPos.I][currentPos.J+x] == 0 && (currentPos.I != lastPos.I || currentPos.J + x != lastPos.J) )
+    {
+      newPos = {"I":currentPos.I,"J":currentPos.J+x};
+      return newPos;
+    }
   }
 
-//}
+  // check if going up is ok
+  if (currentPos.I-x >= 0)
+  {
+    // Now check if this position is different from last pos
+    if (maze[currentPos.I-x][currentPos.J] == 0 && (currentPos.I-x != lastPos.I || currentPos.J != lastPos.J) )
+    {
+      newPos = {"I":currentPos.I-x,"J":currentPos.J};
+      return newPos;
+    }
+  }
+
+
+  // check if going left is ok
+  if (currentPos.J-x >= 0)
+  {
+    // Now check if this position is different from last pos
+    if (maze[currentPos.I][currentPos.J-x] == 0 && (currentPos.I != lastPos.I || currentPos.J - x != lastPos.J) )
+    {
+      newPos = {"I":currentPos.I,"J":currentPos.J-x};
+      return newPos;
+    }
+  }
+
+
+  return false;
+
+}
+
 
 // Build the main function with the maze
 function main()
