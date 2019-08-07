4a3a423fc938a5e2f13a08f39879b80b5b3422b3
Serge Ayissi <ayissi.serge@gmail.com>
2019-08-07 15:41:38 -0400

    Update separate_words2.js

--- string/separate_words2.js
+++ string/separate_words2.js
@@ -4,9 +4,11 @@ function separateWords(phrase,words)
 {
 
   let testPhrase = phrase;
-  let wordIter = 1;
+  let wordIter = 0;
   let checkPoint = 0;
   let wordRegister = [];
+  let wordList;
+  let wordFromLetter = "";
 
   // First sort the list of words from largest to smallest
   words.sort(function(a,b){
@@ -14,30 +16,87 @@ function separateWords(phrase,words)
   });
 
   // Now go through the phrase char by char and based on the letters check if the word is not already in the group of words
-  while (wordIter <= testPhrase.length)
+  while (wordIter <= testPhrase.length - 1)
   {
 
     // Check the very first character of that phrase and look for a corresponding word starting with that letter
-    firstLetter = testPhrase.substr(0,wordIter);
+    wordFromLetter = findWord(words,phrase.substr(wordIter,1));
 
+    // If found words starting with that letter test them to see if they fit the position starting at wordIter
+    if (wordFromLetter.length >0)
+    {
+
+      wordFromLetter.forEach(function(element){
+        if (phrase.indexOf(element) == wordIter){
+          wordRegister.push({"word":element,"fromPosition":wordIter});
+        }
+      });
+
+
+    }
     wordIter++;
   }
 
+  // Buidld a new matrix with only the zero elements and build the word from them
+  let wordFull = wordRegister.filter(function(element){
+    return element.fromPosition == 0;
+  });
 
-  return words;
+
+  // Do a while loop adding element and remvoving the ones that cannot be paired goiung forward
+  while (wordRegister.length > 1)
+  {
+    for(m=0;m<=wordRegister.length - 1;m++)
+    {
+
+
+    }
+
+  }
+
+  
+  
+  // Do a while loop adding element and remvoving the ones that cannot be paired goiung forward
+  while (wordRegister.length > 1)
+  {
+    for(m=0;m<=wordRegister.length - 1;m++)
+    {
+
+
+    }
+
+  }
+
+  //wordList = findWord(words,phrase.substr(0,1));
+
+  return wordRegister;
+  //return wordList;
+  //return words;
 
 }
 
+
+ // Find words beginning from a letter and return
+function findWord(words,letter)
+{
+  let selection = [];
+  selection = words.filter(function(element){
+    return element.substr(0,1) == letter;
+  });
+
+  return selection;
+
+}
 
 // Build the function main
 function main()
 {
 
-  var phrase = "thequickbrownfoxjumpoverlazydog";
-  var words = ["the", "quick", "brown", "fox", "jump", "over", "lazy", "dog"];
+  //var phrase = "thequickbrownfoxjumpoverlazydog";
+  var words = ["the", "quick", "brown", "fox", "jump", "over", "lazy", "dog", "theme", "then", "need", "for", "speed"];
 
   var phrase = "theneedforspeed";
-  var words = ["the","then","need","for","speed"];
+  //var words = ["the","then","need","for","speed"];
 
   console.log(separateWords(phrase,words));
 
@@ -48,3 +107,4 @@ function main()
 // MAIN--------------------------------------------------------------------------------
 
 main();
+
