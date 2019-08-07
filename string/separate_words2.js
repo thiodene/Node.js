36a5382699b4123b98d5c0a9b7229de7e123ed81
Serge Ayissi <ayissi.serge@gmail.com>
2019-08-07 18:55:10 -0400

    Update separate_words2.js

--- string/separate_words2.js
+++ string/separate_words2.js
@@ -11,9 +11,9 @@ function separateWords(phrase,words)
   let wordFromLetter = "";
 
   // First sort the list of words from largest to smallest
-  words.sort(function(a,b){
-    return b.length - a.length;
-  });
+  //words.sort(function(a,b){
+    //return b.length - a.length;
+  //});
 
   // Now go through the phrase char by char and based on the letters check if the word is not already in the group of words
   while (wordIter <= testPhrase.length - 1)
@@ -28,7 +28,7 @@ function separateWords(phrase,words)
 
       wordFromLetter.forEach(function(element){
         if (phrase.indexOf(element) == wordIter){
-          wordRegister.push({"word":element,"fromPosition":wordIter});
+          wordRegister.push({"Word":element,"fromPosition":wordIter, "toPosition":wordIter+element.length});
         }
       });
 
@@ -37,26 +37,41 @@ function separateWords(phrase,words)
     wordIter++;
   }
 
+  console.log(wordRegister);
+
+
   // Buidld a new matrix with only the zero elements and build the word from them
   let wordFull = wordRegister.filter(function(element){
     return element.fromPosition == 0;
   });
 
-
+  // The end of the word contruction has to be equal to the number of words starting with 0
+  let completePhrase = wordFull.length;
+  let m;
+  let n;
   // Do a while loop adding element and remvoving the ones that cannot be paired goiung forward
-  while (wordRegister.length > 1)
-  {
-    for(m=0;m<=wordRegister.length - 1;m++)
+  //while (wordRegister.length > completePhrase)
+  //{
+    for(m=0;m<=wordFull.length - 1;m++)
     {
-
+      for(n=0;n<=wordRegister.length - 1;n++)
+      {
+        if (wordFull[m].toPosition == wordRegister[n].fromPosition)
+        {
+           wordFull[m].toPosition = wordRegister[n].toPosition;
+           wordFull[m].Word += " " + wordRegister[n].Word;
+        }
+      }
 
     }
 
-  }
+  //}
 
   //wordList = findWord(words,phrase.substr(0,1));
 
-  return wordRegister;
+  //return wordRegister;
+  //console.log(wordRegister);
+  return wordFull;
   //return wordList;
   //return words;
 
@@ -75,12 +90,64 @@ function findWord(words,letter)
 
 }
 
+
+  // Buidld a new matrix with only the zero elements and build the word from them
+  let wordFull = wordRegister.filter(function(element){
+    return element.fromPosition == 0;
+  });
+
+  // The end of the word contruction has to be equal to the number of words starting with 0
+  let completePhrase = wordFull.length;
+  let m;
+  let n;
+  // Do a while loop adding element and remvoving the ones that cannot be paired goiung forward
+  //while (wordRegister.length > completePhrase)
+  //{
+    for(m=0;m<=wordFull.length - 1;m++)
+    {
+      for(n=0;n<=wordRegister.length - 1;n++)
+      {
+        if (wordFull[m].toPosition == wordRegister[n].fromPosition)
+        {
+           wordFull[m].toPosition = wordRegister[n].toPosition;
+           wordFull[m].Word += " " + wordRegister[n].Word;
+        }
+      }
+
+    }
+
+  //}
+
+  //wordList = findWord(words,phrase.substr(0,1));
+
+  //return wordRegister;
+  //console.log(wordRegister);
+  return wordFull;
+  //return wordList;
+  //return words;
+
+}
+
+
+ // Find words beginning from a letter and return
+function findWord(words,letter)
+{
+  let selection = [];
+  selection = words.filter(function(element){
+    return element.substr(0,1) == letter;
+  });
+
+  return selection;
+
+}
+
+
 // Build the function main
 function main()
 {
 
   //var phrase = "thequickbrownfoxjumpoverlazydog";
-  var words = ["the", "quick", "brown", "fox", "jump", "over", "lazy", "dog", "theme", "then", "need", "for", "speed"];
+  var words = ["the", "quick", "brown", "fox", "jump", "over", "jumpover", "lazy", "dog", "theme", "then", "need", "for", "speed"];
 
   var phrase = "theneedforspeed";
   //var words = ["the","then","need","for","speed"];
