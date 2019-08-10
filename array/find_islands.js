98cfa9af9eac132b82dca83340a68744ade9889d
Serge Ayissi <ayissi.serge@gmail.com>
2019-08-10 10:06:07 -0400

    Create find_islands.js

---
+++ array/find_islands.js
@@ -0,0 +1,30 @@
+'use strict';
+// Rergister the island from a matrix
+function registerIslands(island)
+{
+
+
+
+
+
+
+  return island;
+
+}
+
+
+// Build the main function
+function main()
+{
+
+  var island = [[1,1,0,0,0],[0,1,0,0,1],[1,0,0,1,1],[0,0,0,0,0],[1,0,1,0,1]];
+  console.log(registerIslands(island));
+
+
+
+}
+
+
+// MAIN---------------------------------------------------------------------------------------
+
+main();
