39a45b494d9bdc37f695e308b9c208ad11f7b6a2
Serge Ayissi <ayissi.serge@gmail.com>
2019-08-23 12:19:37 -0400

    Update kruskal.js

--- minimum_spanning_tree/kruskal.js
+++ minimum_spanning_tree/kruskal.js
@@ -2,41 +2,113 @@
 // https://www.tutorialspoint.com/Kruskal-s-algorithm-in-Javascript
 
 //Now let us look at the implementation of Kruskal's algorithm using this data structure:
-kruskalsMST() {
-   // Initialize graph that'll contain the MST
-   const MST = new Graph();
-   this.nodes.forEach(node => MST.addNode(node));
-   if (this.nodes.length === 0) {
+'use strict';
+
+const UnionFind = require('./union_find_class.js');
+const PriorityQueue = require('./priority_queue_class.js');
+
+class Graph
+{
+  constructor()
+  {
+    this.edges = {};
+    this.nodes = [];
+  }
+
+  addNode(node)
+  {
+    this.nodes.push(node);
+    this.edges[node] = [];
+  }
+
+  addEdge(node1, node2, weight)
+  {
+    weight = typeof weight  !== 'undefined' ? weight : 1;
+    this.edges[node1].push({ node: node2, weight: weight });
+    this.edges[node2].push({ node: node1, weight: weight });
+  }
+
+  addDirectedEdge(node1, node2, weight)
+  {
+    weight = typeof weight  !== 'undefined' ? weight : 1;
+    this.edges[node1].push({ node: node2, weight: weight });
+  }
+
+  // Graph without Weight
+  //addEdge(node1, node2)
+  //{
+      //this.edges[node1].push(node2);
+      //this.edges[node2].push(node1);
+  //}
+
+  //addDirectedEdge(node1, node2)
+  //{
+     //this.edges[node1].push(node2);
+  //}
+
+  display()
+  {
+    let graph = "";
+    this.nodes.forEach(node => {
+      graph += node + "->" + this.edges[node].map(n => n.node).join(", ") + "\n";
+    });
+    console.log(graph);
+  }
+
+  //------------------KRUSKAL
+
+  kruskalsMST()
+  {
+
+    // Initialize graph that'll contain the MST
+    const MST = new Graph();
+    this.nodes.forEach(node => MST.addNode(node));
+    if (this.nodes.length === 0)
+    {
       return MST;
-   }
+    }
 
-   // Create a Priority Queue
-   edgeQueue = new PriorityQueue(this.nodes.length * this.nodes.length);
+    // Create a Priority Queue
+    let edgeQueue = new PriorityQueue(this.nodes.length * this.nodes.length);
 
-   // Add all edges to the Queue:
-   for (let node in this.edges) {
-      this.edges[node].forEach(edge => {
-         edgeQueue.enqueue([node, edge.node], edge.weight);
+    // Add all edges to the queue
+    for (let node in this.edges)
+    {
+      this.edges[node].forEach (edge => {
+        edgeQueue.enqueue([node, edge.node], edge.weight);
       });
-   }
 
-   let uf = new UnionFind(this.nodes);
+    }
 
-   // Loop until either we explore all nodes or queue is empty
-   while (!edgeQueue.isEmpty()) {
-      // Get the edge data using destructuring
+    let uf = new UnionFind(this.nodes);
+
+    // Loop until either we explore all nodes or queue is empty
+    while (!edgeQueue.isEmpty())
+    {
+      // Get the edge data using destructing
       let nextEdge = edgeQueue.dequeue();
-      let nodes = nextEdge.data;
+      //console.log(nextEdge);
+      let nodes = nextEdge.element;
+      //console.log(nodes);
       let weight = nextEdge.priority;
+      //console.log(weight);
 
-      if (!uf.connected(nodes[0], nodes[1])) {
-         MST.addEdge(nodes[0], nodes[1], weight);
-         uf.union(nodes[0], nodes[1]);
+      if(!uf.connected(nodes[0], nodes[1]))
+      {
+        MST.addEdge(nodes[0], nodes[1], weight);
+        uf.union(nodes[0], nodes[1]);
       }
-   }
-   return MST;
+
+    }
+    return MST;
+  }
+
+
 }
-You can test this using:
+
+
+
+//You can test this using:
 
 let g = new Graph();
 g.addNode("A");
