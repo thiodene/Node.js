d6afc3b1b1c91830338b6602ae92e3d74ea1da4e
Serge Ayissi <ayissi.serge@gmail.com>
2019-08-23 10:50:05 -0400

    Update graph_weight.js

--- minimum_spanning_tree/graph_weight.js
+++ minimum_spanning_tree/graph_weight.js
@@ -1,70 +1,78 @@
 'use strict';
 
+const UnionFind = require('./union_find_class.js');
+const PriorityQueue = require('./priority_queue_class.js');
+
 class Graph
 {
-
-  constructor(noOfVertices)
+  constructor()
   {
-    this.noOfVertices = noOfVertices;
-    this.AdjList = new Map();
+    this.edges = {};
+    this.nodes = [];
   }
 
-  // Add Vertices to the Graph
-  addVertex(v)
+  addNode(node)
   {
-    this.AdjList.set(v, []);
+    this.nodes.push(node);
+    this.edges[node] = [];
   }
 
-  // Add edge to the Graph
-  addEdge(v, w, x)
+  addEdge(node1, node2, weight)
   {
-    // Add the weight value for both Edges
-    this.AdjList.get(v).push({"Edge":w,"Weight":x});
-    this.AdjList.get(w).push({"Edge":v,"Weight":x});
+    weight = typeof weight  !== 'undefined' ? weight : 1;
+    this.edges[node1].push({ node: node2, weight: weight });
+    this.edges[node2].push({ node: node1, weight: weight });
   }
 
-  printGraph()
+  addDirectedEdge(node1, node2, weight)
   {
-    // get all the Vertices
-    var get_keys = this.AdjList.keys();
+    weight = typeof weight  !== 'undefined' ? weight : 1;
+    this.edges[node1].push({ node: node2, weight: weight });
+  }
 
-    // Iterate over the vertices
-    for (var i of get_keys)
-    {
-      var get_values = this.AdjList.get(i);
-      var conc = "";
+  // Graph without Weight
+  //addEdge(node1, node2)
+  //{
+      //this.edges[node1].push(node2);
+      //this.edges[node2].push(node1);
+  //}
 
-      for (var j of get_values)
-        conc += j.Edge + "(" + j.Weight + ")" + " " ;
+  //addDirectedEdge(node1, node2)
+  //{
+     //this.edges[node1].push(node2);
+  //}
 
-      console.log(i + " -> " + conc);
-
-    }
-
+  display()
+  {
+    let graph = "";
+    this.nodes.forEach(node => {
+      graph += node + "->" + this.edges[node].map(n => n.node).join(", ") + "\n";
+    });
+    console.log(graph);
   }
 
 }
 
-
 //----------------------------------------------------------------------
 
-var g = new Graph(6);
-var vertices = ["A","B","C","D","E","F"];
+var g = new Graph();
+g.addNode("A");
+g.addNode("B");
+g.addNode("C");
+g.addNode("D");
+g.addNode("E");
+g.addNode("F");
+g.addNode("G");
 
-g.addVertex(vertices[0]);
-g.addVertex(vertices[1]);
-g.addVertex(vertices[2]);
-g.addVertex(vertices[3]);
-g.addVertex(vertices[4]);
-g.addVertex(vertices[5]);
 
-g.addEdge('A','B',10);
-g.addEdge('A','D',5);
-g.addEdge('A','E',20);
-g.addEdge('B','C',16);
-g.addEdge('D','E',7);
-g.addEdge('E','F',1);
-g.addEdge('E','C',9);
-g.addEdge('C','F',12);
+g.addEdge("A","C",100);
+g.addEdge("A","B",3);
+g.addEdge("A","D",4);
+g.addEdge("C","D",3);
+g.addEdge("D","E",8);
+g.addEdge("E","F",10);
+g.addEdge("B","G",9);
+g.addEdge("E","G",50);
 
-g.printGraph();
+g.display();
+
