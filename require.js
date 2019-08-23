5ab6c3f1227b27c6f1c925db9a18a1a5e9cc7568
Serge Ayissi <ayissi.serge@gmail.com>
2019-08-23 10:27:43 -0400

    Create require.js

---
+++ require.js
@@ -0,0 +1,24 @@
+// Require classes and functions from secondary JS file
+
+// MAIN File---------------------------------------------------------
+'use strict';
+
+const UnionFind = require('./union_find_class.js');
+const PriorityQueue = require('./priority_queue_class.js');
+
+//------------------------------------------------------
+
+
+
+
+// CLASS File--------------------------------------------------------
+'use strict';
+
+// PriorityQueue class
+class PriorityQueue
+{
+  //------------------------------------------------------
+  //------------------------------------------------------
+}
+
+module.exports = PriorityQueue;
