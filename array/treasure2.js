df131d4b8493817e2fb804d76309d0ebc026cedd
Serge Ayissi <ayissi.serge@gmail.com>
2019-08-28 12:00:58 -0400

    Update treasure2.js

--- array/treasure2.js
+++ array/treasure2.js
@@ -11,6 +11,9 @@ function find_treasure(mapArray)
   let cursorPos = {};
   let lastPos = {};
   let direction = {"I":0,"J":0};
+  let g;
+  let s;
+  let hasMoved ;
 
   // Register all the starting and goal positions
   for (let i=0;i<=mapArray.length - 1;i++)
@@ -26,18 +29,21 @@ function find_treasure(mapArray)
     }
   }
 
+
   // For each goal position, go through the start position and get the smallest path to reach the goals
   for(g=0;g<=goalPos.length - 1;g++)
   {
     for(s=0;s<=startPos.length - 1; s++)
     {
-      cursorPos = {"I":startPos.I,"J":startPos.J};
-      lastPos = {"I":startPos.I,"J":startPos.J};
+      cursorPos = {"I":startPos[s].I,"J":startPos[s].J};
+      console.log(cursorPos);
+      lastPos = {"I":startPos[s].I,"J":startPos[s].J};
       foundPos = false;
       moves = 0;
       while (foundPos != true)
       {
-
+        // Initialize the move
+        hasMoved = false;
         // First get the natural direction to reach goal on I
         if (cursorPos.I < goalPos[g].I)
           direction.I = 1;
@@ -53,62 +59,208 @@ function find_treasure(mapArray)
           direction.J = -1;
         else
           direction.J = 0;
-
-        // If could not get closerr on I or J try go another direction that is not the direction you came from
-
-
-        // Try to get closer on I
-         if (cursorPos.I + 1 <= mapArray.length - 1)
-         {
-           if (mapArray[cursorPos.I + 1] != "D")
-           {
-
-           }
-           else
-           {
-
-
-           }
-         }
-
-         // Try to get closer on J
-         if (cursorPos.J + 1 <= mapArray[0].length - 1)
-         {
-
-         }
-
-         // Try to get closer on I
-         if (cursorPos.I - 1 >= 0)
-         {
-           cursorPos.I += 1;
-         }
-
-         // Try to get closer on J
-         if (cursorPos.J - 1 >= 0)
-         {
-
-         }
+        // Try a move in those directions and check if it is possible
+        if (direction.I > 0)
+        {
+          // Verify it doesn't go out of limits
+          if (cursorPos.I + direction.I <= mapArray.length - 1)
+          {
+            // Now verify the new position is allowed!
+            if (mapArray[cursorPos.I + direction.I][cursorPos.J] != "D")
+            {
+              // Add the condition for last position
+              if (lastPos.I != cursorPos.I + direction.I || lastPos.J != cursorPos.J)
+              {
+                lastPos = cursorPos;
+                console.log(lastPos);
+                cursorPos.I = cursorPos.I + direction.I;
+                hasMoved = true;
+              }
+            }
+          }
+        }
 
 
+
+        // Try a move in those directions and check if it is possible
+        if (direction.I < 0)
+        {
+          // Verify it doesn't go out of limits
+          if (cursorPos.I + direction.I >= 0)
+          {
+            // Now verify the new position is allowed!
+            if (mapArray[cursorPos.I + direction.I][cursorPos.J] != "D")
+            {
+              // Add the condition for last position
+              if (lastPos.I != cursorPos.I + direction.I || lastPos.J != cursorPos.J)
+              {
+                lastPos = cursorPos;
+                console.log(lastPos);
+                cursorPos.I = cursorPos.I + direction.I;
+                hasMoved = true;
+              }
+            }
+          }
+        }
+
+        if (!hasMoved)
+        {
+          // Try a move in those directions and check if it is possible
+          if (direction.J > 0)
+          {
+            // Verify it doesn't go out of limits
+            if (cursorPos.J + direction.J <= mapArray[0].length - 1)
+            {
+              // Now verify the new position is allowed!
+              if (mapArray[cursorPos.I][cursorPos.J + direction.J] != "D")
+              {
+                // Add the condition for last position
+                if (lastPos.I != cursorPos.I || lastPos.J != cursorPos.J + direction.J)
+                {
+                  lastPos = cursorPos;
+                  cursorPos.J = cursorPos.J + direction.J;
+                  hasMoved = true;
+                }
+              }
+            }
+          }
+
+
+          // Try a move in those directions and check if it is possible
+          if (direction.J < 0)
+          {
+            // Verify it doesn't go out of limits
+            if (cursorPos.J + direction.J >= 0)
+            {
+              // Now verify the new position is allowed!
+              if (mapArray[cursorPos.I][cursorPos.J + direction.J] != "D")
+              {
+                // Add the condition for last position
+                if (lastPos.I != cursorPos.I || lastPos.J != cursorPos.J + direction.J)
+                {
+                  lastPos = cursorPos;
+                  cursorPos.J = cursorPos.J + direction.J;
+                  hasMoved = true;
+                }
+              }
+            }
+          }
+        }
+        // If no move has been made to get the cursor closer choose another direction that isnot lastPos!
+        // choose another direction on I or on J
+        if (!hasMoved)
+        {
+          console.log("Not!");
+          // If direction.I equals zero then move on I left or right
+          if (Math.abs(cursorPos.I - goalPos[g].I) >= Math.abs(cursorPos.J - goalPos[g].J))
+          {
+            console.log("closer on J");
+            // Verify it doesn't go out of limits
+            if (cursorPos.J - 1 > 0)
+            {
+              // Now verify the new position is allowed!
+              if (mapArray[cursorPos.I][cursorPos.J - 1] != "D")
+              {
+                // Add the condition for last position
+                if (lastPos.I != cursorPos.I || lastPos.J != cursorPos.J - 1)
+                {
+                  lastPos = cursorPos;
+                  console.log(lastPos);
+                  cursorPos.J = cursorPos.J - 1;
+                  hasMoved = true;
+                }
+              }
+            }
+
+            if (!hasMoved)
+            {
+              // Verify it doesn't go out of limits
+              if (cursorPos.J + 1 > mapArray[0].length)
+              {
+                // Now verify the new position is allowed!
+                if (mapArray[cursorPos.I][cursorPos.J + 1] != "D")
+                {
+                  // Add the condition for last position
+                  if (lastPos.I != cursorPos.I || lastPos.J != cursorPos.J + 1)
+                  {
+                    lastPos = cursorPos;
+                    console.log(lastPos);
+                    cursorPos.J = cursorPos.J - 1;
+                    hasMoved = true;
+                  }
+                }
+              }
+
+            }
+
+          }
+          else
+          {
+            console.log("closer on I");
+            // If closer on I move I farther
+
+            // Verify it doesn't go out of limits
+            if (cursorPos.I - 1 > 0)
+            {
+              // Now verify the new position is allowed!
+              if (mapArray[cursorPos.I - 1][cursorPos.J] != "D")
+              {
+                // Add the condition for last position
+                if (lastPos.I != cursorPos.I - 1 || lastPos.J != cursorPos.J)
+                {
+                  lastPos = cursorPos;
+                  console.log(lastPos);
+                  cursorPos.I = cursorPos.I - 1;
+                  hasMoved = true;
+                }
+              }
+            }
+
+            if (!hasMoved)
+            {
+              // Verify it doesn't go out of limits
+              if (cursorPos.I + 1 > mapArray.length)
+              {
+                // Now verify the new position is allowed!
+                if (mapArray[cursorPos.I + 1][cursorPos.J] != "D")
+                {
+                  // Add the condition for last position
+                  if (lastPos.I != cursorPos.I + 1 || lastPos.J != cursorPos.J)
+                  {
+                    lastPos = cursorPos;
+                    console.log(lastPos);
+                    cursorPos.I = cursorPos.I + 1;
+                    hasMoved = true;
+                  }
+                }
+              }
+
+            }
+
+
+          }
+
+        }
+
+        //if (foundPos)
+        //  break;
+        break;
       }
 
     }
 
   }
 
-
-
-  return minimalMoves;
+  //console.log(lastPos);
+  return cursorPos;
+  //return minimalMoves;
 
   //return startPos;
   return goalPos;
 
 
-
-
-
-
 }
+
 
 
 
@@ -116,14 +268,22 @@ function find_treasure(mapArray)
 function main()
 {
 
-  var route = [["S","O","O","S","S"],
-               ["D","O","D","O","D"],
-               ["O","O","O","O","X"],
-               ["X","D","D","O","O"],
-               ["X","D","D","D","O"]];
+  var mapArray = [["O","O","O","O","S"],
+                  ["D","O","D","O","D"],
+                  ["O","O","O","O","X"],
+                  ["O","D","D","O","O"],
+                  ["O","D","D","D","O"]];
 
 
-  console.log(find_treasure(route));
+
+  //var mapArray = [["S","O","O","S","S"],
+  //                ["D","O","D","O","D"],
+  //                ["O","O","O","O","X"],
+  //                ["X","D","D","O","O"],
+  //                ["X","D","D","D","O"]];
+
+
+  console.log(find_treasure(mapArray));
 
 
 
@@ -135,3 +295,4 @@ function main()
 
 
 main();
+
