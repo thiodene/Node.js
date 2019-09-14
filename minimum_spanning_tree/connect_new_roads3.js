4b8957dd95be8c769f8ccdbc0c89e7b62c60f80e
Serge Ayissi <ayissi.serge@gmail.com>
2019-09-14 10:18:38 -0400

    Create connect_new_roads3.js

---
+++ minimum_spanning_tree/connect_new_roads3.js
@@ -0,0 +1,185 @@
+'use strict';
+
+const UnionFind = require('./union_find_class.js');
+const PriorityQueue = require('./priority_queue_class.js');
+
+class Graph
+{
+  constructor()
+  {
+    this.edges = {};
+    this.nodes = [];
+  }
+
+  addNode(node)
+  {
+    this.nodes.push(node);
+    this.edges[node] = [];
+  }
+
+  addEdge(node1, node2, weight)
+  {
+    weight = typeof weight  !== 'undefined' ? weight : 1;
+    this.edges[node1].push({ node: node2, weight: weight });
+    this.edges[node2].push({ node: node1, weight: weight });
+  }
+
+  addDirectedEdge(node1, node2, weight)
+  {
+    weight = typeof weight  !== 'undefined' ? weight : 1;
+    this.edges[node1].push({ node: node2, weight: weight });
+  }
+
+  display()
+  {
+    let graph = "";
+    this.nodes.forEach(node => {
+      graph += node + "->" + this.edges[node].map(n => n.node).join(", ") + "\n";
+    });
+    console.log(graph);
+  }
+
+  //------------------KRUSKAL
+
+  kruskalsMST()
+  {
+
+    // Initialize graph that'll contain the MST
+    const MST = new Graph();
+    this.nodes.forEach(node => MST.addNode(node));
+    if (this.nodes.length === 0)
+    {
+      return MST;
+    }
+
+    // Create a Priority Queue
+    let edgeQueue = new PriorityQueue(this.nodes.length * this.nodes.length);
+
+    // Add all edges to the queue
+    for (let node in this.edges)
+    {
+      this.edges[node].forEach (edge => {
+        edgeQueue.enqueue([node, edge.node], edge.weight);
+      });
+
+    }
+
+    let uf = new UnionFind(this.nodes);
+
+    // Loop until either we explore all nodes or queue is empty
+    while (!edgeQueue.isEmpty())
+    {
+      // Get the edge data using destructing
+      let nextEdge = edgeQueue.dequeue();
+      //console.log(nextEdge);
+      let nodes = nextEdge.element;
+      //console.log(nodes);
+      let weight = nextEdge.priority;
+      //console.log(weight);
+
+      if(!uf.connected(nodes[0], nodes[1]))
+      {
+        MST.addEdge(nodes[0], nodes[1], weight);
+        uf.union(nodes[0], nodes[1]);
+      }
+
+    }
+    return MST;
+  }
+
+
+}
+
+
+//----------------------------------------------------------------------
+
+function main()
+{
+
+  var n = 3;
+  var newEdges =  [[1,2,5],[1,3,6],[2,3,1]] ;
+  var oldEdges = [];
+
+  //var n = 4;
+  //var newEdges = [[1,2,3],[3,4,4]];
+  //var oldEdges = [];
+
+  //var n = 6;
+  //var oldEdges = [[1,4],[4,5],[2,3]];
+  //var newEdges = [[1,2,5],[1,3,10],[1,6,2],[5,6,5]];
+
+  var oldCost = 0;
+  // Add the old edges to new edges
+  oldEdges.forEach(function(element){
+    newEdges.push([element[0],element[1],1]);
+    oldCost++;
+  });
+
+//  console.log(newEdges);
+//  return false;
+
+  var connectedCost = 0;
+  var mstCost = 0;
+  var miniCost = 0;
+  let j;
+
+
+
+  // First verify if the total amount of edges is at least n - 1 vertices!
+  if (newEdges.length < n - 1)
+  {
+    console.log(-1);
+    return -1;
+  }
+  else if (newEdges.length == n - 1)
+  {
+    // If the number of Edges equals the number of nodes - 1 => we have an MST
+    // Caculate directly the MST
+    newEdges.forEach(function(element){
+      mstCost += element[2];
+    });
+  }
+  else
+  {
+
+    // First Add nodes to a new Graph
+    var g = new Graph();
+    for(let i=1;i<=n;i++)
+    {
+      g.addNode(String(i));
+    }
+
+    // Now add the Edges to connecte for building an MST
+    newEdges.forEach(function(element){
+      g.addEdge(String(element[0]),String(element[1]), element[2]);
+    });
+
+    g.display();
+    // Create the Kruskal MST Graph and save it
+    let gk = g.kruskalsMST();
+
+    gk.display();
+
+    // Calculated the new connected cost after MST Kruskal
+    gk.nodes.forEach(function(element){
+      for(j=0;j<=gk.edges[element].length - 1;j++)
+      {
+        mstCost += gk.edges[element][j].weight;
+        console.log(gk.edges[element][j]);
+      }
+      //console.log(gk.edges[element]);
+      //mstCost += gk.edges[element].weight;
+    });
+
+    miniCost = mstCost/2 - oldCost;
+    console.log(miniCost);
+    return miniCost;
+
+  }
+
+}
+
+
+//---------------------------------------------------------------------------------
+
+main();
