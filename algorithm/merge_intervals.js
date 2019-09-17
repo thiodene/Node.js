eb511276fe93493483738ff969e2822221e5e6c3
Serge Ayissi <ayissi.serge@gmail.com>
2019-09-17 16:15:01 -0400

    Create merge_intervals.js

---
+++ algorithm/merge_intervals.js
@@ -0,0 +1,29 @@
+
+// Function to check if time slot is inside an interval of time
+function mergeInterval(interval1,interval2)
+{
+
+  let newInterval = [];
+  // Check the overlap of intervals
+  if (interval1[0] >= interval2[0] && interval1[1] <= interval2[1])
+  {
+    return newInterval = [interval2[0],interval2[1]];
+  }
+
+  if(interval2[0] >= interval1[0] && interval2[1] <= interval1[1])
+  {
+    return newInterval = [interval1[0],interval1[1]];
+  }
+
+  if (interval1[0] <= interval2[0] && interval1[1] >= interval2[0])
+  {
+    return newInterval = [interval1[0],interval2[1]];
+  }
+
+  if (interval2[0] >= interval1[0] && interval2[0] <= interval1[1])
+  {
+    return newInterval = [interval1[0],interval2[1]];
+  }
+
+  return false;
+}
