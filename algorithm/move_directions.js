88d19447604f4a7a8539b5eb92096427e51fa63d
Serge Ayissi <ayissi.serge@gmail.com>
2019-10-09 15:20:28 -0400

    Create move_directions.js

---
+++ algorithm/move_directions.js
@@ -0,0 +1,7 @@
+// 4 Directions with Loop
+let directions = [{"I":1,"J":0},{"I":0,"J":1},{"I":-1,"J":0},{"I":0,"J":-1}];
+
+
+// 8 Directions with Loop
+let directions = [{"I":1,"J":0},{"I":0,"J":1},{"I":-1,"J":0},{"I":0,"J":-1},
+                  {"I":1,"J":1},{"I":1,"J":-1},{"I":-1,"J":1},{"I":-1,"J":-1}];
