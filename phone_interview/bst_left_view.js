c0b05b500bf6a146f1a20cdeb4dd349e98933a47
Serge Ayissi <ayissi.serge@gmail.com>
2019-10-15 09:14:01 -0400

    Update bst_left_view.js

--- phone_interview/bst_left_view.js
+++ phone_interview/bst_left_view.js
@@ -1,9 +1,21 @@
   // LEFT VIEW
-  this.left_view = function(root)
+  this.max_level = 0;
+  this.view_list = [];
+  this.left_view = function(root,level)
   {
     if (root === null )
       return;
 
+    if (this.max_level < level)
+    {
+      //console.log(root.data);
+      this.view_list.push(root.data);
+      this.max_level = level;
+    }
 
-
+    this.left_view(root.left,level+1);
+    this.left_view(root.right,level+1);
+    //return false;
+    return this.view_list;
   }
+
