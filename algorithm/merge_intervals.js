e638d2c68ca1e1f70834c8f2fbf836c12d27b564
Serge Ayissi <ayissi.serge@gmail.com>
2020-05-01 16:42:14 -0400

    Update merge_intervals.js

--- algorithm/merge_intervals.js
+++ algorithm/merge_intervals.js
@@ -1,29 +1,28 @@
-
 // Function to check if time slot is inside an interval of time
 function mergeInterval(interval1,interval2)
 {
-
-  let newInterval = [];
-  // Check the overlap of intervals
-  if (interval1[0] >= interval2[0] && interval1[1] <= interval2[1])
-  {
+    let newInterval = [];
+    // Check the overlap of intervals
+    if (interval1[0] >= interval2[0] && interval1[1] <= interval2[1])
+    {
     return newInterval = [interval2[0],interval2[1]];
-  }
+    }
 
-  if(interval2[0] >= interval1[0] && interval2[1] <= interval1[1])
-  {
+    if(interval2[0] >= interval1[0] && interval2[1] <= interval1[1])
+    {
     return newInterval = [interval1[0],interval1[1]];
-  }
+    }
 
-  if (interval1[0] <= interval2[0] && interval1[1] >= interval2[0])
-  {
+    //---------------------------------------------------------------------------
+    if (interval1[0] < interval2[0] && interval1[1] >= interval2[0] && interval1[1] < interval2[1])
+    {
     return newInterval = [interval1[0],interval2[1]];
-  }
+    }
 
-  if (interval2[0] >= interval1[0] && interval2[0] <= interval1[1])
-  {
-    return newInterval = [interval1[0],interval2[1]];
-  }
+    if (interval2[0] < interval1[0] && interval1[0] <= interval2[1]  && interval2[1] < interval1[1])
+    {
+    return newInterval = [interval2[0],interval1[1]];
+    }
 
-  return false;
+    return false;
 }
